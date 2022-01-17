3e80c627a629efcc9188432ef0340f9e026a330f
tylertaewook <70506321+tylertaewook@users.noreply.github.com>
2022-01-17 17:20:21 +0900

    finished scheduler/test_models.py

--- tutor_scheduler/scheduler/tests/test_models.py
+++ tutor_scheduler/scheduler/tests/test_models.py
@@ -7,7 +7,7 @@ from tutor_scheduler.scheduler.tests.factories import SessionFactory
 pytestmark = pytest.mark.django_db
 
 
-class TestModel:
+class TestSessionModel:
     def test_is_upcoming_with_future_date(self):
         """
         is_upcoming() returns True for sessions scheduled in the future
@@ -27,3 +27,11 @@ class TestModel:
             date=datetime.date.today() + datetime.timedelta(days=deltadays)
         )
         assert not past_session.is_upcoming()
+
+    def test_get_weekday(self):
+        weekday_session = SessionFactory()
+        assert weekday_session.get_weekday == datetime.date.today().strftime("%A")
+
+    def test_get_absolute_url(self):
+        url_session = SessionFactory()
+        assert url_session.get_absolute_url() == f"/sessions/{url_session.pk}/"
